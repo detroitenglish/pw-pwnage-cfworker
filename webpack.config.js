39874ba40a9d5155b294eaba30dc29d37a893213
Dave Willenberg <dave@detroit-english.de>
2018-08-08 02:39:56 +0200

    Set defaults in build step; inject timestamp for last-modified header

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,12 @@ require('dotenv').config({ path: __dirname + '/cloudflare.env' })
 
 const webpack = require('webpack')
 
-const { ALLOWED_ORIGIN, ALWAYS_RETURN_SCORE, CORS_MAXAGE } = process.env
+const {
+  ALLOWED_ORIGIN = '*',
+  ALLOWED_ORIGIN_PATTERNS = '',
+  ALWAYS_RETURN_SCORE = false,
+  CORS_MAXAGE = 300,
+} = process.env
 
 module.exports = {
   entry: './src/index.js',
@@ -20,8 +25,10 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       ALLOWED_ORIGIN: JSON.stringify(ALLOWED_ORIGIN),
+      ALLOWED_ORIGIN_PATTERNS: JSON.stringify(ALLOWED_ORIGIN_PATTERNS),
       ALWAYS_RETURN_SCORE: JSON.stringify(!!ALWAYS_RETURN_SCORE),
       CORS_MAXAGE: JSON.stringify(CORS_MAXAGE),
+      LAST_MODIFIED: JSON.stringify(new Date().toJSON()),
     }),
   ],
 }
