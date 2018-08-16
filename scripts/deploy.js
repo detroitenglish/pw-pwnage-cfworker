6c432b095bebc70dfc6528ade495aed3bb58390b
Dave Willenberg <dave@detroit-english.de>
2018-08-16 22:44:06 +0200

    Remove unnecessary JS validation; add non-zero exit on failure

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,10 @@
 require('dotenv').config({ path: __dirname + '/../cloudflare.env' })
 const axios = require('axios')
 const fs = require('fs')
-const esprima = require('esprima')
 
 const { AUTH_EMAIL, AUTH_KEY, ZONE_ID } = process.env
 ;(async function init() {
-  const script = fs
-    .readFileSync(__dirname + '/../dist/index.js', 'utf8')
-    .toString()
-
-  console.info(`Script is valid JS? ${isValidJS(script)}`)
+  const script = fs.readFileSync(__dirname + '/../dist/index.js', 'utf8')
 
   const result = await axios({
     method: `PUT`,
@@ -19,24 +14,12 @@ const { AUTH_EMAIL, AUTH_KEY, ZONE_ID } = process.env
       'x-auth-email': AUTH_EMAIL,
       'x-auth-key': AUTH_KEY,
     },
-    data: Buffer.from(script),
+    data: script,
   }).catch(err => {
     console.error(JSON.stringify(err.response.data, null, 2))
-    // return process.exit(1)
+    process.exit(1)
   })
   if (result.data.success) {
     return console.info(`Worker upload successful 🚀`)
   }
 })()
-
-function isValidJS(str) {
-  let isValid
-  try {
-    esprima.parseScript(str)
-    isValid = true
-  } catch (err) {
-    console.error(err)
-    isValid = false
-  }
-  return isValid
-}
